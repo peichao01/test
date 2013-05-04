98ef6b2ec559a998f67136ea0a903f1f2c2c6608
peichao01 <380692952@qq.com>
2013-05-04 19:57:50 +0800

    up

---
+++ ios/HelloWorld/HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by 裴超 on 13-5-4.
+//  Copyright (c) 2013年 裴超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/HelloWorld/HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by 裴超 on 13-5-4.
+//  Copyright (c) 2013年 裴超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
