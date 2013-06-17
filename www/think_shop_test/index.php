afe7eb592ef1ccbe20e8c5fd5bf38e59f16c5386
peichao01 <380692952@qq.com>
2013-06-17 17:08:19 +0800

    thinkshop, thinkPHP test

---
+++ www/think_shop_test/index.php
@@ -0,0 +1,12 @@
+<?php
+
+define('APP_NAME', 'Index');
+define('APP_PATH', './Index/');
+define('RUNTIME_PATH', APP_PATH.'Temp/');
+
+//部署项目时去掉这句话
+define('APP_DEBUG', true);
+
+include './ThinkPHP/ThinkPHP.php';
+
+?>
