86e4aacf4aa206a4962a9e80617ad15894eb5950
peichao01 <380692952@qq.com>
2013-07-01 09:40:43 +0800

    cocos2d-x test

---
+++ ios/cocos2dxTest/cocos2dxTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  cocos2dxTestAppController.h
+//  cocos2dxTest
+//
+//  Created by 裴超 on 13-6-30.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
